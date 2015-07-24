855c3def40a5705e986160ed1a263aec83f58a10
James Haff <jehaff@James-Haffs-MacBook-Pro.local>
2015-07-24 15:45:29 -0500

    my first server

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+Bundler.require()
+
+def movie(name, release_year, description)
+  {:name => name.to_s, :release_year => release_year.to_s, :description => description.to_s}.to_json
+end
+
+get '/' do
+  cross_origin
+  movie("CaddyShack", "1992", "Bill Murray is a Gopher Killer... RAH RAH RAH!")
+end
+
+get '/api/movie_name1' do
+  cross_origin
+  movie("Rambo", "1998", "Slyvester Kills EvERYONENNENNNEE... AHHH BITCHEs DIeeee!")
+end
+
+get '/api/movie_name2' do
+  cross_origin
+  movie("Sandlot", "1994", "You PLAY ball Like A GIRL")
+end
